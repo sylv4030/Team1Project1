c27f72834172ec7f401fe76e37201bdd6e014aa7
Karla Goo <karla_goo@yahoo.com>
2021-07-15 17:27:16 -0500

    added drop-down

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-
-   document.addEventListener('DOMContentLoaded', function() {     
-       var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('select');
+   var instances = M.FormSelect.init(elems, {});
  });
-
- $('.dropdown-trigger').dropdown();
-
-  console.log("hello");
-
-  alert(window.$?1:0)
